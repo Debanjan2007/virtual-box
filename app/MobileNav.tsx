8b6ede2cde5591d5d46d36e2e5c618cf780d47c8
Debanjan Das <microsoft2420@gmail.com>
2026-06-24 01:05:48 +0530

    Add nav bar and side to the landing page

---
+++ app/MobileNav.tsx
@@ -0,0 +1,39 @@
+﻿// MobileMenu.tsx
+"use client"
+
+import Link from 'next/link'
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger,
+} from "@/components/ui/sheet";
+
+export function MobileMenu() {
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="ghost" size="icon">
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+
+            <SheetContent side="right">
+                <div className="flex flex-col gap-6 mt-8 m-4">
+                    <Link href="/features">Features</Link>
+                    <Link href="/pricing">Pricing</Link>
+                    <Link href="/docs">Docs</Link>
+
+                    <Button variant="outline">
+                        Login
+                    </Button>
+
+                    <Button>
+                        Get Started
+                    </Button>
+                </div>
+            </SheetContent>
+        </Sheet>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,33 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import Image from "next/image";
+import { Menu } from 'lucide-react'
+import { MobileMenu } from "./MobileNav"
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-col flex-1 items-center justify-start bg-zinc-50 font-sans dark:bg-[#08101D] overflow-auto">
+      <nav className="h-18 w-full border-b">
+        <div className="navbar mx-auto h-full w-full px-6 flex items-center justify-between">
+          <div className="logo hidden md:flex text-xl gap-2 font-bold text-white">
+            <Image src="/logo.png" alt="logo" width='30' height='10' className="rounded-md" />
+            <p>ShortLy</p>
+          </div>
+          <div className="md:hidden">
+            <MobileMenu/>
+          </div>
+          <div className="navigation hidden md:flex gap-8 items-center">
+            <Link href="/features">Features</Link>
+            <Link href="/pricing">Pricing</Link>
+            <Link href="/doc">Docs</Link>
+          </div>
+          <div className=" action-buttons flex gap-4">
+            <Button variant="outline" size="lg">Login</Button>
+            <Button variant="default" size="lg">Get started</Button>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </nav>
     </div>
   );
 }
