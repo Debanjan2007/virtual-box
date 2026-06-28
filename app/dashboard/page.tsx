1c49dfcc0720937b6284b87557f28faa19ba9bde
Debanjan Das <microsoft2420@gmail.com>
2026-06-28 20:19:14 +0530

    Add dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+﻿"use client"
+
+export default function Dashboard(){
+    return (
+        <div className="container flex justify-center items-center">
+            <p className="text-5xl">Dashboard</p>
+        </div>
+    )
+}
