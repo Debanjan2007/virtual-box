647dc2ffb58d18419fdf31a07b1670d205654550
Debanjan Das <microsoft2420@gmail.com>
2026-06-29 16:51:47 +0530

    Add db schema and modeled it

---
+++ models/User.model.js
@@ -0,0 +1,37 @@
+﻿import { Schema , model } from "mongoose";
+
+const UserSchema = new Schema({
+    clerkUid: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    firstname: {
+        type: String,
+        unique: false,
+        required: [true || "Name is a required field"]
+    },
+    lastname: {
+        type: String,
+        unique: false,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: false
+    },
+    email:{
+        type: String,
+        required: [true || "Email is a required"],
+        unique: true
+    },
+    isLogedin: {
+        type: Boolean,
+        default: false
+    }
+},{
+    timestamps: true
+})
+
+
+export const User = model("User" , UserSchema)
