be9398807f9ebc2d1f4436d857b462e10dd2f2c2
Debanjan Das <microsoft2420@gmail.com>
2026-06-30 10:03:31 +0530

    Update refactor signup redirect url path and server side validation

---
+++ app/auth/signup/callback/page.tsx
@@ -0,0 +1,22 @@
+﻿import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+export default async function GET(){
+    let shouldRedirectTosignUp = false
+    try {
+        const user = await currentUser()
+        console.log("user is :",user)
+        if(!user){
+            shouldRedirectTosignUp = true
+        }
+    } catch (error) {
+        console.log(error)
+        shouldRedirectTosignUp = true
+    }
+
+    if(shouldRedirectTosignUp){
+        redirect("/signup")
+    }else{
+        redirect("/dashboard")
+    }
+}
