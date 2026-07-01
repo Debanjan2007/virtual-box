45cebd051ec77c6c5ea4773ff8f8b1850d7b6f78
Debanjan Das <microsoft2420@gmail.com>
2026-07-01 21:43:21 +0530

    Add nav contents with responsive design

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,10 @@
 ﻿import { AnimatedGridPattern } from "@/components/ui/animated-grid-pattern"
-import { MobileMenu } from "./MobileMenu"
+import { MobileMenu } from "./Components/MobileMenu"
 import Image from "next/image";
 import { Bell, Settings, User } from 'lucide-react';
 import Link from "next/link";
+import { SearchBar } from "./Components/Search"
+import { ControllPanel } from "./Components/ControllPanel"
 
 export default function Layout({
     children
@@ -12,6 +14,23 @@ export default function Layout({
     return <>
         <div className="container h-screen w-screen flex flex-col">
             <AnimatedGridPattern className="absolute inset-0 opacity-35 text-slate-500/60 dark:text-slate-400/40" />
+            <nav>
+                <div className="container w-full h-14 flex justify-between items-center px-4">
+                    <div className="logo-sec flex items-center gap-2 hidden md:flex">
+                        <Image src="/logo.png" alt="logo" height={36} width={36}/>
+                        <h1 className="text-lg font-semibold">VBox</h1>
+                    </div>
+                    <div className="monbile-menu md:hidden flex items-center justify-center">
+                        <MobileMenu />
+                    </div>
+                    <div className="search w-full md:w-1/2 h-full flex items-center justify-center">
+                        <SearchBar />
+                    </div>
+                    <div className="controll-options flex items-center gap-2">
+                        <ControllPanel />
+                    </div>
+                </div>
+            </nav>
             <div className="children w-full h-full">
                 {children}
             </div>
